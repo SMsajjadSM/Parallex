3dcbc5eac77ff585d32a9b020e988e696a4c690e
sajjad <sajjadsiadati1390@gmail.com>
2024-02-01 17:38:31 +0330

    add tlw

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        parallex: "#003329",
+        text: "#359381",
+      },
+    },
+  },
+  plugins: [],
+};
